3c3fa185b5b38d30295f54f751f8bd346478afdb
Tyler Adam Martinez <tylermartinez@my.unt.edu>
2022-10-26 07:26:18 -0500

    [Client] Fix connection issues

--- ESP32Code/Client.ino
+++ ESP32Code/Client.ino
@@ -1,12 +1,12 @@
 /*
  HTTP Client
 
- This program drives a brush and brushless DC motor.
- The motor is attached to digital pins 3 and 9 of the Arduino.
+ This program gets information from a server.
 
- The motor spins at Full rotation speed for 2 seconds,
- then half the rotation speed for 2 seocnds,
- and then no movement from motor for 2 seconds. 
+ It connects to the server using HTTPS GET method.
+ The HTTP response's body is a JSON object. The program 
+ deserializes the JSON object and uses that data to light an 
+ RGB LED to UNT Green.
 
  Created October 25, 2022
  by Tyler Adam Martinez 
@@ -16,22 +16,17 @@
 #include <WiFi.h>
 #include <ArduinoJson.h>
 #include <stdlib.h>
-#include <HttpClient.h>
+#include <WiFiClientSecure.h>
+#include <HTTPClient.h>
 
 const int LED = 13; // LED pin for debugging
 const int RED_PIN = 33;
 const int BLUE_PIN = 25;
 const int GREEN_PIN = 26;
 
-const int INPUT_BTN = 32; // Input from from button push
-
 // Set WiFi details
 const char *ssid = "SSID"; // Put your SSID here
 const char *password = "PASSWORD"; // Put your PASSWORD here
-
-// Name of the server we want to connect to
-const char kHostname[] = "arduino.cc";
-const char kPath[] = "/";
 
 void enableDebugLED() {
     digitalWrite(LED, HIGH);
@@ -43,42 +38,94 @@ void disableDebugLED() {
     Serial.println(F("Turning debug LED to OFF"));
 }
 
+void turnOnRGB(int r, int g, int b) {
+    analogWrite(RED_PIN, r);
+    analogWrite(GREEN_PIN, g);
+    analogWrite(BLUE_PIN, b);
+    Serial.println(F("Turning RGB LED to ON"));
+    
+    // Displaying RBG values to Serial Monitor
+    Serial.print(F("Values => R:"));
+    Serial.print(F(r));
+    Serial.print(F(" G:"));
+    Serial.print(F(g));
+    Serial.print(F(" B:"));
+    Serial.println(F(b));
+}
+
+void turnOffRGB() {
+    analogWrite(RED_PIN, 0);
+    analogWrite(GREEN_PIN, 0);
+    analogWrite(BLUE_PIN, 0);
+    Serial.println(F("Turning RGB LED to OFF"));
+}
+
+void sendRequest() {
+    WiFiClientSecure client;
+    HTTPClient http;
+    
+    // Send request
+    http.useHTTP10(true);
+    http.begin(client, "https://raw.githubusercontent.com/TylerAdamMartinez/Hardware-Design/master/Assets/unt_green.json");
+    http.GET();
+    
+    // Parse response
+    DynamicJsonDocument doc(2048);
+    deserializeJson(doc, http.getStream());
+    
+    // Read values
+    Serial.println(http.getString());
+    int redValue = doc["RGB"][0].as<int>();
+    int greenValue = doc["RGB"][2].as<int>();
+    int blueValue = doc["RGB"][1].as<int>();
+    
+
+    // Disconnect
+    http.end();
+    
+    // Turn RGB LED ON
+    turnOnRGB(redValue, greenValue, blueValue);
+
+    // Delay
+    delay(1000);
+}
+
 void setup() {    
-  // Set the LED pins as an outputs
-  pinMode(LED, OUTPUT);
+    // Set the LED pins as an outputs
+    pinMode(LED, OUTPUT);
 
-  // Set the RGB LED pins as an outputs
-  pinMode(RED_PIN, OUTPUT);
-  pinMode(GREEN_PIN, OUTPUT);
-  pinMode(BLUE_PIN, OUTPUT);
+    // Set the RGB LED pins as an outputs
+    pinMode(RED_PIN, OUTPUT);
+    pinMode(GREEN_PIN, OUTPUT);
+    pinMode(BLUE_PIN, OUTPUT);
 
-  Serial.begin(115200);
- 
-  if (!strcmp(ssid, "SSID")) {
-    Serial.println(F("Please set your WiFi SSID"));
-    while(1);
-  }
-  if (!strcmp(password, "PASSWORD")) {
-    Serial.println(F("Please set your WiFi PASSWORD"));
-    while(1);
-  }
+    Serial.begin(115200);
+  
+    if (!strcmp(ssid, "SSID")) {
+      Serial.println(F("Please set your WiFi SSID"));
+      while(1);
+    }
+    if (!strcmp(password, "PASSWORD")) {
+      Serial.println(F("Please set your WiFi PASSWORD"));
+      while(1);
+    }
 
-  // Connect to WiFi network
-  Serial.print(F("Connecting to "));
-  Serial.println(F(ssid));
+    // Connect to WiFi network
+    Serial.print(F("Connecting to "));
+    Serial.println(F(ssid));
 
-  WiFi.begin(ssid, password);
-  while (WiFi.status() != WL_CONNECTED) {
-    delay(750);
-    Serial.println(F("."));
-  }
-  Serial.print(F("WiFi connected: "));
-  Serial.println(WiFi.localIP());
+    WiFi.begin(ssid, password);
+    while (WiFi.status() != WL_CONNECTED) {
+      delay(750);
+      Serial.println(F("."));
+    }
+    Serial.print(F("WiFi connected: "));
+    Serial.println(WiFi.localIP());
 
 }
 
 void loop() {
-  // SEND HTTP REQUEST
-
-  delay(1000);
+    sendRequest();
+    turnOffRGB();
+    delay(1000);
 }
